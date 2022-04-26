8c45dac41e51e463c19daa14cf816bf567e977ce
Duzhinsky <w1vvv1w@ya.ru>
2022-04-26 13:41:15 +0300

    simple config implementation

--- src/main/java/ru/duzhinsky/preorderbot/Main.java
+++ src/main/java/ru/duzhinsky/preorderbot/Main.java
@@ -1,7 +1,9 @@
 package ru.duzhinsky.preorderbot;
 
+import ru.duzhinsky.preorderbot.data.Config;
+
 public class Main {
     public static void main(String[] args) {
-
+        Config.list(System.out);
     }
 }
--- src/main/java/ru/duzhinsky/preorderbot/data/Config.java
+++ src/main/java/ru/duzhinsky/preorderbot/data/Config.java
@@ -1,7 +1,41 @@
 package ru.duzhinsky.preorderbot.data;
 
-public class Config {
-    static {
+import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Iterator;
+import java.util.Properties;
+
+public class Config {
+    private static final Properties props = new Properties();
+
+    static {
+        try {
+            String json = Files.readString(Path.of("src/main/java/ru/duzhinsky/preorderbot/data/config.json"));
+            JSONObject configObject = new JSONObject(json);
+            Iterator<String> keys = configObject.keys();
+            while(keys.hasNext()) {
+                String key = keys.next();
+                Object value = configObject.get(key);
+                props.setProperty(key,value.toString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String key) {
+        return props.getProperty(key);
+    }
+
+    public static String getProperty(String key, String defaultValue) {
+        return props.getProperty(key, defaultValue);
+    }
+
+    public static void list(PrintStream out) {
+        props.list(out);
     }
 }
